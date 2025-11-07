4c0c3d6bdda8bf03f4cea697dc811c5f53f1a3a9
MouseManBoi <152597806+MouseManBoi@users.noreply.github.com>
2025-11-07 16:00:15 -0500

    IT WORKS IT WORKS IT WORKS 2

--- src/main/java/net/baconeater/ModServer.java
+++ src/main/java/net/baconeater/ModServer.java
@@ -24,11 +24,6 @@ import org.slf4j.LoggerFactory;
 import java.util.Collection;
 
 public class ModServer implements ModInitializer {
-	public static final String MOD_ID = "creepershader";
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
 
