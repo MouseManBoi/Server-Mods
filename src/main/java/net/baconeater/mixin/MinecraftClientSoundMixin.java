a22c58c30ea360dd7e625bc0ff102463ae5f4c3f
MouseManBoi <152597806+MouseManBoi@users.noreply.github.com>
2026-05-25 03:24:09 -0400

    dimension change sfx

---
+++ src/main/java/net/baconeater/mixin/MinecraftClientSoundMixin.java
@@ -0,0 +1,24 @@
+package net.baconeater.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.SoundManager;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientSoundMixin {
+    @Redirect(
+            method = "setWorld",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/sound/SoundManager;stopAll()V"
+            )
+    )
+    private void server_mods$keepSoundsOnDimensionChange(SoundManager soundManager, ClientWorld world) {
+        if (world == null) {
+            soundManager.stopAll();
+        }
+    }
+}
